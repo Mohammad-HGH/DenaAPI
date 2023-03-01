7d824817473e9eb4bbe26c6255c4e82320ed8bff
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-03-01 21:26:52 +0330

    categories entities completed

---
+++ Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DenaAPI.Entities
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        public int ParentID { get; set; }
+
+
+    }
+}
