e7bac62287ed46e15d9f5482460e51b0e697ca4b
faezeh-vahedi <118221831+faezeh-vahedi@users.noreply.github.com>
2023-03-02 22:16:01 +0330

    Intermediary ,attribute & product

---
+++ DTO/IntermediaryAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace DenaAPI.DTO
+{
+    public class IntermediaryAttribute
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int IntermediaryKey { get; set; }
+    }
+}
---
+++ DTO/IntermediaryProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace DenaAPI.DTO
+{
+    public class IntermediaryProduct
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int IntermediaryKey { get; set; }
+    }
+}
