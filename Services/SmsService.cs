31b691b655dc0a3f34b678df2e74dbc5540e2a8a
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-03-07 01:23:03 +0330

    user can login with phone number

--- Services/SmsService.cs
+++ Services/SmsService.cs
@@ -10,10 +10,6 @@ namespace DenaAPI.Services
     public class SmsService : ISmsService
     {
         private readonly DenaDbContext denaDbContext;
-
-
-
-
         public SmsService(DenaDbContext denaDbContext)
         {
             this.denaDbContext = denaDbContext;
@@ -56,65 +52,13 @@ namespace DenaAPI.Services
         public async Task<SmsResponse> SmsVerifyAsync(SmsRequest smsRequest)
         {
             var existingPhone = await denaDbContext.Sms.SingleOrDefaultAsync(sms => sms.SmsId == smsRequest.SmsCode && sms.UserId == smsRequest.UserId);
-            /* 
-             var messageSendObject = new MessageSendObject()
-             {
-                 Messages = new List<string> { $"بترکی!! این دفه منم با کد تایید!! {smsCode}" }.ToArray(),
-                 MobileNumbers = new List<string> { smsRequest.Phone }.ToArray(),
-                 LineNumber = "300070797197",
-                 SendDateTime = null,
-                 CanContinueInCaseOfError = true
-             };
-             if (existingPhone != null)
-             {
-                 return new SmsResponse
-                 {
-                     Success = false,
-                     Error = "User already exists with the same phone",
-                     ErrorCode = "S02"
-                 };
-             }
-             MessageSendResponseObject messageSendResponseObject = new MessageSend().Send(token, messageSendObject);
-             if (messageSendResponseObject.IsSuccessful)
-             {
-                 var sms = new Sms
-                 {
-                     Phone = smsRequest.Phone,
-                     SmsId = int.Parse(smsCode),
-                     UserId = smsRequest.UserId,
-                     TS = DateTime.Now,
-                 };
-                 denaDbContext.Sms.Add(sms);
-
-                 var saveResponse = await denaDbContext.SaveChangesAsync();
-
-                 if (saveResponse >= 0)
-                 {
-                     return new SmsResponse { Success = true, Phone = sms.Phone };
-                 }
-
-                 return new SmsResponse
-                 {
-                     Error = "ok",
-                     ErrorCode = "200",
-                     Success = true,
-                 };
-             }
-             else
-             {
-                 return new SmsResponse
-                 {
-                     Success = false,
-                     Message = "Sms not sent!"
-                 };
-             }*/
             if (existingPhone == null)
             {
                 return new SmsResponse
                 {
                     Success = false,
-                    Error = "Phone not found",
-                    ErrorCode = "L02"
+                    Error = "Sms code not valid!",
+                    ErrorCode = "500"
                 };
             }
 
